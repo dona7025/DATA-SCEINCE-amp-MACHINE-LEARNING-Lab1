c5d391043d7eaed377e8d68de3a493665737291a
dona7025 <101088786+dona7025@users.noreply.github.com>
2022-11-16 07:20:25 +0530

    Add files via upload

--- L4Part2P3.py
+++ L4Part2P3.py
@@ -1,8 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import  pandas as pd
-from sklearn.model_selection import  train_test_split
-from  sklearn.preprocessing import  StandardScaler
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import confusion_matrix
 from sklearn.metrics import accuracy_score
